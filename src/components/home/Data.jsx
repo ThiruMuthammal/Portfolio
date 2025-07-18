ac5383c1daa9911e4b3691accf054bf730e2e8c4
ThiruMuthammal <thirumuthammal1297@gmail.com>
2025-07-18 10:57:44 +0530

    title change

--- src/components/home/Data.jsx
+++ src/components/home/Data.jsx
@@ -3,12 +3,12 @@ import React from "react";
 const Data = () => {
     return (
         <div className="home__data">
-            <h1 className="home__title">Thirumoorthy
+            <h1 className="home__title">Thirumoorthy M
                 {/* <img src={img1} className="home__hand" /> */}
                 <img src="/hand.svg" className="home__hand" />
 
             </h1>
-            <h3 className="home__subtitle">Full Stack Developer</h3>
+            <h3 className="home__subtitle">Developer</h3>
             <p className="home__description">I'm a passionate Full Stack Developer with experience in designing developing and deploying scalable web application using React, .Net Core, and SQL Server.
             </p>
             <a href="#contact" className="button button--flex">
